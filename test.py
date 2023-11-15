6f10d4e1d1b77d7460e4a8f8894ea6595fe1dbf5
Duda Elbakidze <147228294+duda-el@users.noreply.github.com>
2023-11-15 23:18:53 +0400

    Update test.py

--- test.py
+++ test.py
@@ -3,21 +3,21 @@
 # search mobile numbers and names
 
 
-contact = {"Duda" : "592505750",
+phonebook = {"Duda" : "592505750",
            "Gela" : "597603450",
            "Mariami" : "577402490",
            "Alexi" : "595115427"}
 
 
 while True:
-    choose = print("\n 1. Seacrh contact \n 2. Add contact \n 3. Delete contact \n 4. Update contact \n 5. Display contacts \n")
+    choose = print("\n 1. Seacrh contact \n 2. Add contact \n 3. Delete contact \n 4. Update contact \n 5. Display phonebook \n")
     choose_1 = int(input("What you would like to do? "))
 
     
     if choose_1 == 1:
         search_name = input("Please enter name or part of a name: ")
         found = False
-        for name,number in contact.items():
+        for name,number in phonebook.items():
             if search_name.lower() in name.lower():
                 print(f"{name}'s contact number is {number}")
                 found = True
@@ -25,16 +25,14 @@ while True:
                 print(f"No contact found for {search_name.capitalize()}")
     elif choose_1 == 2:
         add_name = input("Please enter name: ")
-        add_contact = input("Please enter contact: ")
-        dict_add = contact[add_name.capitalize()] = add_contact
+        add_phonebook = input("Please enter contact: ")
+        dict_add = phonebook[add_name.capitalize()] = add_phonebook
     elif choose_1 == 3:
         delete_name = input("Please enter name or part of a name: ")
-        if delete_name in contact:
-            contact.pop(delete_name)
-        elif delete_name not in contact:
+        if delete_name.capitalize() in phonebook:
+            phonebook.pop(delete_name.capitalize())
+        elif delete_name.lower() not in phonebook.lower():
              print(f"No contact found for {delete_name.capitalize()}")
     elif choose_1 == 5:
-         for name,number in sorted(contact.items()):
+         for name,number in sorted(phonebook.items()):
               print(f"{name} - {number}")
-        
-        
